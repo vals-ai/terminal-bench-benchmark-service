e43f48df5b4786ca38d7b6605de13aa8fe47f051
Jarett Forzano <jarett@vals.ai>
2026-08-15 23:18:00 -0700

    chore: trim migration diff

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from pytest import Config
-
-
-def pytest_configure(config: Config) -> None:
-    """Configure pytest with the auth-disabled test profile."""
-    os.environ["AUTH_DISABLED"] = "true"
